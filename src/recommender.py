97a7851c79b4028ea124f3472693f411d9762c01
TheSachsquatch <vsachin@umich.edu>
2022-01-08 21:56:28 -0500

    back button misc small fixes

--- src/recommender.py
+++ src/recommender.py
@@ -60,12 +60,18 @@ def getRecs(genre, song_name):
     from selenium.webdriver.common.by import By
     from selenium.webdriver.chrome.options import Options
     sims = sims.iloc[:10]
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    except:
+        s=Service(ChromeDriverManager().install())
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options,service=s)
     sim_short = sims[5:]
     for i in range(5):
         videos.append("none")
